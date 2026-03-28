5887e2e158679124f188fe5e51384ae249463c55
LO1304 <cheikhmouhamadoumoustaphalo@gmail.com>
2026-03-28 06:51:43 +0100

    Fix erreur 500: Ajout de collectstatic dans build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ pip install -r requirements.txt
 
 python manage.py migrate
 
+# Collecte des fichiers statiques pour WhiteNoise et Cloudinary
+python manage.py collectstatic --no-input
+
 # Création de l'admin (remplace par tes infos)
 if [ "$CREATE_SUPERUSER" ]; then
   python manage.py createsuperuser --no-input --username "moustapha" --email "cheikhmouhamadoumoustaphalo@gmail.com"
