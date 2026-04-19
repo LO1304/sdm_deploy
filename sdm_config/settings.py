5adcfb054115fe3cee4bff4afab80b3bc6cb046d
LO1304 <cheikhmouhamadoumoustaphalo@gmail.com>
2026-04-19 19:17:58 +0200

    Activation Cloudinary pour les Medias sur PythonAnywhere

--- sdm_config/settings.py
+++ sdm_config/settings.py
@@ -108,13 +108,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 MEDIA_URL   = '/media/'
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 
-# Stockage : Cloudinary uniquement sur Render, local sinon
-if 'RENDER' in os.environ:
-    STATICFILES_STORAGE  = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+# Stockage : Cloudinary sur Render et PA, local sinon
+if env('CLOUDINARY_CLOUD_NAME', default='local') != 'local':
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
 else:
-    STATICFILES_STORAGE  = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
+# Fichiers Statiques : Cloudinary sur Render, WhiteNoise (ultra-rapide) sur PythonAnywhere
+if 'RENDER' in os.environ:
+    STATICFILES_STORAGE  = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+else:
+    STATICFILES_STORAGE  = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ── AUTRES ──
 DEFAULT_AUTO_FIELD  = 'django.db.models.BigAutoField'
