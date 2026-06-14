f381c7a0a6c529fa6b577751706561a09b3ad66a
LO1304 <cheikhmouhamadoumoustaphalo@gmail.com>
2026-06-14 14:34:52 +0200

    Fix: move cloudinary_storage below staticfiles in INSTALLED_APPS to prevent collectstatic hijack

--- sdm_config/settings.py
+++ sdm_config/settings.py
@@ -60,13 +60,13 @@ AUTHENTICATION_BACKENDS = [
 
 # ── APPLICATIONS ──
 INSTALLED_APPS = [
-    'cloudinary_storage',   # TOUJOURS en premier
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
     'bibliotheque',
     'rest_framework',
